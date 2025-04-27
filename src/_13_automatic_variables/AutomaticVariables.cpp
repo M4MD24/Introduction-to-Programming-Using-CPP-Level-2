ee9c2fe3960569fb5c30b632766aab8af1f1b344
m4md24 <m4md24@gmail.com>
2025-04-27 19:32:03 +0300

    # Learn Automatic Variables

---
+++ src/_13_automatic_variables/AutomaticVariables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const auto INTEGER_NUMBER = 2;
+    const auto LONG_NUMBER = 2L;
+    const auto CHARACTER = '2';
+    const auto TEXT = "Two";
+    const auto FLOAT_NUMBER = 2.2F;
+    const auto DOUBLE_NUMBER = 2.2;
+    const auto BOOLEAN = true;
+}
